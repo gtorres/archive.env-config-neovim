bfa6f7d62eebc09357191794c709ca29c5ea29c3
Gerard Torres <g.m.torres@outlook.com>
2023-02-18 11:47:51 -0700

    fix: <leader>pv was mapped twice
    To the same key with different syntax. y tho?

--- lua/gtorres/remap.lua
+++ lua/gtorres/remap.lua
@@ -1,6 +1,4 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("n", "<leader>u", ":UndotreeShow<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
